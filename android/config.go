398295995d6b805529f7d16e8338ec36832f41ea
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-02-20 03:13:42 +0900

    Extends "soong: Treat vendor/lineage-priv/* as release-keys"
    * Expands to all vendor dir
    
    Change-Id: Id9662c6bab4021e9dc865777105c7d16142dbc3a

--- android/config.go
+++ android/config.go
@@ -1056,7 +1056,7 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
-	if strings.HasPrefix(defaultCert, "vendor/lineage-priv/") {
+	if strings.HasPrefix(defaultCert, "vendor/") {
 		return "release-keys"
 	}
 	return "dev-keys"
